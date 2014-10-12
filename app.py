136beab66844826d3251becb389aa6697d086e88
Victor Gaitour <Grimlon1234@gmail.com>
2014-10-12 17:07:12 -0400

    app now completely works with a fake dictionary of results

--- app.py
+++ app.py
@@ -1,14 +1,28 @@
 from flask import Flask, render_template, request
-
-
+import collections
+import soup
 app=Flask(__name__)
 
 @app.route("/",methods=["GET","POST"])
 def index():
     searchstring=request.form.get("search")
     print searchstring
-    return render_template("index.html")
-
+    if searchstring>0:
+        sampleresults=collections.OrderedDict()
+        sampleresults["Toby McGuire"]=27
+        sampleresults["Andrew Garfeild"]=19
+        sampleresults["Peter Parker"]=6
+        sampleresults["Norman Osborne"]=3
+        sampleresults["Mary Jane"]=1
+        #results=soup.findhigh(soup.search(searchstring))
+        #print results
+        return render_template("search.html",results=sampleresults,
+                               searchstring=searchstring)
+    else:
+        return render_template("index.html")
+@app.route("/search",methods=["GET","POST"])
+def search():
+    return index()
 if __name__=="__main__":
     app.debug=True
     app.run()
