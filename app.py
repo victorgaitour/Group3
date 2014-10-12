1ccdc2b29e58d18fd1747646b46c777893e02163
Victor Gaitour <Grimlon1234@gmail.com>
2014-10-12 14:00:17 -0400

    app.py working

--- app.py
+++ app.py
@@ -3,6 +3,11 @@ from flask import Flask, render_template, request
 
 app=Flask(__name__)
 
+@app.route("/",methods=["GET","POST"])
+def index():
+    searchstring=request.form.get("search")
+    print searchstring
+    return render_template("index.html")
 
 if __name__=="__main__":
     app.debug=True
