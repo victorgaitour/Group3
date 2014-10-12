4434ca03a784b8d203b3504c73611c273ad9fc3d
koprty <koprty@users.noreply.github.com>
2014-10-12 15:52:30 -0400

    added placeholder for when functs

--- soup.py
+++ soup.py
@@ -8,6 +8,7 @@ import urllib2
 
 question = "Who is the president of the United States today?"
 
+#returns a dictionary with the search results
 def search(question):
     g =  google.search(question, num = 10, start = 0, stop = 10, pause=3.0)
     utils = [w for w in g]
@@ -19,13 +20,13 @@ def search(question):
     else:
         name = False
         
-    count = 0;
     for x in utils:
     #run beautiful soup to find names
         try:
             html_doc = google.get_page(x)
             soup = BeautifulSoup(html_doc)
             y = soup.get_text()
+
             if name:
                 names = check_names(get_potential_names(y))
                 #names = findname(y)
@@ -37,7 +38,11 @@ def search(question):
                     #find highest number of names
                    # dhigh=findhigh(d);
                     #return addition of that 
-                count +=1
+            else: #when regex
+                #do when regex here
+
+
+
         except Exception, error:
             print error
             
@@ -85,7 +90,9 @@ g.googlesearch("Question", num = 10, start =0, stop = 10)
 utils [x for x in g]
 print utils
 
-Results:
+Results: (who testing with:
+question = "Who is the president of the United States today?"
+)
 //nameFinder runs alot faster than namere.py//
 probably b/c namere.py looks for all kinds of different names like those including middle names and stuff
 1)namere.py
